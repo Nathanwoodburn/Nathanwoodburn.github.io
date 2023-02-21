293f2f4f5c34bd41afe3db652a7f899cf98e4ea7
Nathan Woodburn <github@nathan.woodburn.au>
2023-02-21 22:20:17 +1100

    about: added scroll animation and cleaned old links

---
+++ assets/js/about.js
@@ -0,0 +1,30 @@
+function slideout() {
+    bodydiv = document.querySelector("body");
+    bodydiv.style.top = "-100%";
+    setTimeout(function() { 
+        window.location.href = "/"
+        bodydiv.style.top = "0px";
+    }, 1000)
+}
+
+
+// jQuery(document).ready(function(){
+//     function resizeForm(){
+//         var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
+//         if(width > 1024){
+
+//         } else {
+
+//         }    
+//     }
+//     window.onresize = resizeForm;
+//     resizeForm();
+// });
+
+document.addEventListener("scroll", scroll);
+function scroll() {
+    var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
+    if(width > 1024){
+        slideout();
+    }
+}
