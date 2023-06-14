8c768f760ec6a5c9e84cc49321a95f77cff46c96
Nathan Woodburn <github@nathan.woodburn.au>
2023-06-14 23:31:32 +1000

    HNS redirect: Fixed redirect with hash

--- handshake.js
+++ handshake.js
@@ -1,5 +1,5 @@
 if (window.location.hostname != document.currentScript.getAttribute('domain')){
-	window.location.href = "https://" + document.currentScript.getAttribute('domain') + window.location.pathname;
+	window.location.href = "https://" + document.currentScript.getAttribute('domain') + window.location.pathname + window.location.search + window.location.hash;
 }
 else{
 	console.log("Already Redirected");
