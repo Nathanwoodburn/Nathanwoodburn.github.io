de476eb7abd83614bee2351573a627d90ffea1a2
Nathan Woodburn <github@nathan.woodburn.au>
2025-01-29 20:53:46 +1100

    fix: Add more cached files to sw

--- pwa/sw.js
+++ pwa/sw.js
@@ -6,7 +6,20 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const PRECACHE_ASSETS = [
   '/',
-  '/404'
+  '/404',
+  '/assets/bootstrap/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&display=swap',
+  'https://fonts.googleapis.com/css?family=Cabin:700&display=swap',
+  'https://fonts.googleapis.com/css?family=Anonymous+Pro&display=swap',
+  'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+  '/assets/css/styles.min.css',
+  '/assets/css/404.min.css',
+  '/assets/css/profile.min.css',
+  '/assets/bootstrap/js/bootstrap.min.js',
+  '/assets/js/script.min.js',
+  '/assets/js/404.min.js',
+  '/assets/img/favicon/favicon-16x16.png',
+  '/assets/img/favicon/android-chrome-192x192.png'
 ]
 
 
