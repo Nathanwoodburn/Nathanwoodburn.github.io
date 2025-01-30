7bea502bdf1c1c75286093ae0fd465b63f6300a8
Nathan Woodburn <github@nathan.woodburn.au>
2025-01-30 13:00:28 +1100

    feat: Add now page for Jan

--- pwa/sw.js
+++ pwa/sw.js
@@ -35,36 +35,3 @@ self.addEventListener('install', async (event) => {
       .then((cache) => cache.addAll(PRECACHE_ASSETS))
   );
 });
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match('/404');
-        return cachedResp;
-      }
-    })());
-  }
-});
